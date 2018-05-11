23d8a8668eb3198b641ddbfb602bd7f86fc23a64
Stein Petter Tokvam <stto1210@gmail.com>
2018-05-11 11:03:52 +0200

    opprydding

--- js/date.js
+++ js/date.js
@@ -2,42 +2,20 @@
 function start(){
   startFetching();
   startTime();
-  startSiteSwitch();
 }
-
-function startSiteSwitch(){
-    var res = " ";
-    var together = "";
-
-    res = textFileReader("newfil.txt");
-    if(res == "2"){
-      window.location.href = "slider.html";
-    }
-    setTimeout(startSiteSwitch, 2000);
-  }
-
-  function textFileReader( filename )
-  {
-      var reader = (window.XMLHttpRequest != null )
-                 ? new XMLHttpRequest()
-                 : new ActiveXObject("Microsoft.XMLHTTP");
-      reader.open("GET", filename, false );
-      reader.send( );
-      return reader.responseText;
-  }
 
 function startTime() {
     //starter å hente datoen og formatere informasjonen til visning
-    var today = new Date();
+    var date = new Date();
 
-    var dayNumber = today.getDay();
-    var date = today.getDate();
-    var month = today.getMonth() + 1;
-    var year = today.getFullYear();
+    var dayNumber = date.getDay();
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
 
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
 
     h = checkTime(h);
     m = checkTime(m);
@@ -47,50 +25,47 @@ function startTime() {
     var dayText = setDay(dayNumber);
 
     //Viser tid/dato på siden
-    document.getElementById('date').innerHTML = dayText + " " + date + ". " + month + " " + year;
+    document.getElementById('date').innerHTML = dayText + " " + day + ". " + month + " " + year;
     document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
 
     var t = setTimeout(startTime, 500);
 }
 
 function checkTime(i) {
-    if (i < 10) {i = "0" + i};
+    if (i < 10) {
+        i = "0" + i
+    };
+
     return i;
 }
 
-function setDay(day){
-	if(day == 1){day = "Mandag"}
-    else if(day == 2){day = "Tirsdag"}
-    else if(day == 3){day = "Onsdag"}
-    else if(day == 4){day = "Torsdag"}
-    else if(day == 5){day = "Fredag"}
-    else if(day == 6){day = "L&#248;rdag"}
-    else if(day == 0){day = "S&#248;ndag"};
-    return day;
+function setDay(dayNumber){
+    switch (dayNumber) {
+        case 1: return "Mandag"
+        case 2: return "Tirsdag"
+        case 3: return "Onsdag"
+        case 4: return "Torsdag"
+        case 5: return "Fredag"
+        case 6: return "Lørdag"
+        case 7: return "Søndag"
+        default: return "ERROR"
+    }
 }
 
 function setMonth(month){
-	if(month == 1){month = "Januar"}
-    else if(month == 2){month = "Februar"}
-    else if(month == 3){month = "Mars"}
-    else if(month == 4){month = "April"}
-    else if(month == 5){month = "Mai"}
-    else if(month == 6){month = "Juni"}
-    else if(month == 7){month = "Juli"}
-    else if(month == 8){month = "August"}
-    else if(month == 9){month = "September"}
-    else if(month == 10){month = "Oktober"}
-    else if(month == 11){month = "November"}
-    else if(month == 12){month = "Desember"};
-    return month;
-}
-
-function textFileToArray( filename )
-{
-    var reader = (window.XMLHttpRequest != null )
-               ? new XMLHttpRequest()
-               : new ActiveXObject("Microsoft.XMLHTTP");
-    reader.open("GET", filename, false );
-    reader.send( );
-    return reader.responseText.split(/(\r\n|\n)/g);
+    switch (month) {
+        case 1: return "Januar"
+        case 2: return "Februar"
+        case 3: return "Mars"
+        case 4: return "April"
+        case 5: return "Mai"
+        case 6: return "Juni"
+        case 7: return "Juli"
+        case 8: return "August"
+        case 9: return "September"
+        case 10: return "Oktober"
+        case 11: return "November"
+        case 12: return "Desember"
+        default: return "ERROR"
+    }
 }
--- js/ruter.js
+++ js/ruter.js
@@ -1,8 +1,7 @@
-
 var slemdal = 3012330;
 var stasjonsveien = 3012335;
 var station_id = 3012330;//starter med slemdal
-//var busOrMetro = "Linje "
+
 var id = ["line", "aimed", "expected"];
 
 function startFetching(){
@@ -25,7 +24,6 @@ function sendRequest(){
 function changeID(i){
   if(station_id == stasjonsveien){
     if(i == 0){
-      //busOrMetro = "Buss "
       id[0] = "46line";
       id[1] = "46aimed";
       id[2] = "46expected";
@@ -35,7 +33,6 @@ function changeID(i){
       id[2] = "46expected2";
     }
   }else if(i == 0){
-    //busOrMetro = "Linje "
     id[0] = "line";
     id[1] = "aimed";
     id[2] = "expected";
@@ -47,46 +44,42 @@ function changeID(i){
 }
 
 function printAll(responseToGET){
-    //behandler informasjon fra ruter for visning
+  //behandler informasjon fra ruter for visning
   var maxResults = 2;
   var currResults = 0;
 
   changeID(0);
-  if(responseToGET.length == 0){//om siste avgang er gått.. denne og else er ny
-      document.getElementById(id[1]).innerHTML = "Siste avgang har gått."
-  }else{
-      for(i = 0; i < responseToGET.length; i ++){
-        if(responseToGET[i] == "\"DirectionName\":\"1\""){//hvilken retning kollektiven går.. 1=sentrum, 2=andre veien
-          currResults++;
 
-          var linjenr = responseToGET[i-1].split(":");
+  for(i = 0; i < responseToGET.length; i ++){
+    if(responseToGET[i] == "\"DirectionName\":\"1\""){//hvilken retning kollektiven går.. 1=sentrum, 2=andre veien
+      currResults++;
 
-          var linjeTall = parseInt(linjenr[1].charAt(1) + linjenr[1].charAt(2));
-          //document.getElementById(id[0]).innerHTML = busOrMetro + linjeTall;//busOrMetro==variabel for å lagre buss og bane nummer
+      var linjenr = responseToGET[i-1].split(":");
 
-          var aimedArrival = responseToGET[i+19].split("T");
-          var timeAimedArrival = aimedArrival[2].split("+");
+      var linjeTall = parseInt(linjenr[1].charAt(1) + linjenr[1].charAt(2));
 
-          document.getElementById(id[1]).innerHTML = timeAimedArrival[0];
+      var aimedArrival = responseToGET[i+19].split("T");
+      var timeAimedArrival = aimedArrival[2].split("+");
 
-          var expectedArrival = responseToGET[i+20].split("T");
-          var timeExpectedArrival = expectedArrival[2].split("+");
+      document.getElementById(id[1]).innerHTML = timeAimedArrival[0];
 
-          document.getElementById(id[2]).innerHTML = timeExpectedArrival[0];
+      var expectedArrival = responseToGET[i+20].split("T");
+      var timeExpectedArrival = expectedArrival[2].split("+");
 
-          changeID(1);
+      document.getElementById(id[2]).innerHTML = timeExpectedArrival[0];
 
-          if(currResults == maxResults){
-            if(station_id == stasjonsveien){
-              station_id = slemdal;
-              break;
-            }
-            station_id = stasjonsveien;
+      changeID(1);
 
-            sendRequest();
-            break;
-          }
+      if(currResults == maxResults){
+        if(station_id == stasjonsveien){
+          station_id = slemdal;
+          break;
         }
+        station_id = stasjonsveien;
+
+        sendRequest();
+        break;
       }
+    }
   }
 }
